8cab70b31439570ae923683dbf063eb34385507e
Lichkeit <57181190+Lichkeit@users.noreply.github.com>
2019-11-27 21:36:00 +0800

    11/27 認識鋼體物理與碰撞觸發等事件

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     [Header("跳躍高度")]
-    [Range(0.1f, 10f)]
-    public float  jumpUp= 0.5f ;
+    [Range(50, 1000)]
+    public int  jumpUp= 200 ;
 
 
-    [Header("墜落速度")]
-    [Range(-0.001f, -0.1f)]
-    //public float down = -0.01f;
+
+
+    [Header("選轉角度")]
+    [Range(1, 360)]
+    public int angle = 10;
 
     [Header("是否死亡")]
-    public bool dead ;
+    public bool isdead ;
 
     public Transform bird;
 
 
+    public GameObject goScore, goGM;  //可以用，來區隔
+
+    //2D鋼體
+    public Rigidbody2D rg2D;
+
+
+    //碰撞事件
+    //Enter  碰到物件時觸發事件
+    //Exit   離開物間時觸發事件
+    //Stay   站在物件時每秒觸發60次
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //print(collision.gameObject.name);
+        //碰撞,遊戲物件,碰撞的名稱
+
+        Dead();
+    }
+
+    //觸發事件
+    //物件觸發開始執行-針對有勾選Trigger的物件
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Dead();
+    }
+
     private void Start()
     {
-        
+       
     }
 
 
-    //private void Update()
-    //{
-       // bird.Translate(0, down  , 0);
-        
-    //}
+    private void Update()
+    {
+        Jump();//每60偵測玩家是否按了Jump的按鍵
+       
+    }
 
 
     /// <summary>
@@ -37,6 +64,34 @@ public class Bird : MonoBehaviour
     private void Jump()
     {
 
+        if (isdead) return;
+        
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //當滑鼠按下左鍵時啟動
+        {
+            goScore.SetActive(true);//  打開/關閉  MOD
+            goGM.SetActive(true);
+
+            //重設所有物理資料  PS要放在最前面不然會一直reast
+            rg2D.Sleep();
+            
+            
+            //2D鋼體地心引力
+            rg2D.gravityScale = 1;
+            
+            //2D鋼體.推力(二維向量)
+            rg2D.AddForce(new Vector2(0,jumpUp ));
+
+            
+
+            
+        }
+
+        //選轉的角度
+        //rg2D.SetRotation(30);
+        
+        //進階選轉角度+前滾動
+        rg2D.SetRotation(angle*rg2D.velocity.y);//只要y不要x
     }
 
     /// <summary>
@@ -44,7 +99,7 @@ public class Bird : MonoBehaviour
     /// </summary>
     private void Dead()
     {
-
+        isdead = true;
     }
     
     /// <summary>
---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+
+    private void Update()
+    {
+
+        //新增按鍵功能
+        print(Input.GetKeyDown("mouse 0"));            //字串取得按鍵
+        print(Input.GetKeyDown(KeyCode.Mouse1));       //列舉取的按鍵  比較推薦用這個
+
+    }
+}
